855e09ee2c71c4172128e7aead5d7b64ceaf118b
mimouse-wkang <wkang@mimouse.net>
2020-12-14 10:25:53 +0800

    1.0.2-alpha: 1.更新文档 2.添加数字转换类

--- src/main/java/com/wkang/security/KSecurityUtil.java
+++ src/main/java/com/wkang/security/KSecurityUtil.java
@@ -10,8 +10,4 @@ public class KSecurityUtil {
 	public static boolean validPassword(String password, String encpassword)throws Exception{
 		return MD5Util.validPassword(password, encpassword);
 	}
-	
-	public static String decode(String inStr)throws Exception{
-		return MD5Util.convertMD5(MD5Util.convertMD5(inStr));
-	}
 }
